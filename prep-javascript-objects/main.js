15e0399ae7504664f7e44782a99474d3cb236bf5
Jeesu Park <jeesupark96@gmail.com>
2021-09-21 20:40:08 -0700

    java objects

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,13 @@
+var person = {
+  firstName: "Jeesu",
+  lastName: "Park",
+  hobby: "basketball",
+};
+console.log(person);
+var firstName = person.firstName
+console.log("The person's name is: " + firstName + ".")
+person.job = "coder";
+console.log("The person's current job is: " + person.job + ".");
+person.previousJob = "pastor";
+console.log("The person's previous job is: " +person.previousJob + ".");
+console.log(person);
